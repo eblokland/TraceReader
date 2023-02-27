71c8518fd0fe37585ec2d838c1ddd676b9f1a5b1
erikbl <erik.blokland@adyen.com>
2023-02-27 17:03:59 +0100

    fix bug in csv writer

--- function_csv_writer.py
+++ function_csv_writer.py
@@ -3,14 +3,17 @@ from typing import List, Any
 from statistical_analysis import Function
 
 csv_header = ['function address', 'name set', 'leaf samples', 'tree samples', 'local probability',
-              'nonlocal probability','local prob interval', 'nonlocal prob interval', 'local runtime', 'nonlocal runtime', 'local energy', 'nonlocal energy',
+              'nonlocal probability', 'local prob interval', 'nonlocal prob interval', 'local runtime',
+              'nonlocal runtime', 'local energy', 'nonlocal energy',
               'mean local power', 'mean nonlocal power', 'MLP interval', 'MNP interval']
 
 
 def write_function(writer, fun: Function):
-    arr: List[Any] = [fun.addr] + list(fun.name_set) + \
-                     [fun.num_leaf_samples, fun.num_samples, fun.local_prob,
-                      fun.nonlocal_prob, fun.local_prob_interval, fun.nonlocal_prob_interval, fun.local_runtime, fun.nonlocal_runtime, fun.local_energy_cost,
+    fun_names = ' .. '.join(fun.name_set)
+    arr: List[Any] = [fun.addr, fun_names,
+                      fun.num_leaf_samples, fun.num_samples, fun.local_prob,
+                      fun.nonlocal_prob, fun.local_prob_interval, fun.nonlocal_prob_interval, fun.local_runtime,
+                      fun.nonlocal_runtime, fun.local_energy_cost,
                       fun.nonlocal_energy_cost, fun.mean_local_power, fun.mean_nonlocal_power,
                       fun.mean_local_power_interval, fun.mean_nonlocal_power_interval]
     writer.writerow(arr)
@@ -22,4 +25,3 @@ def write_csv(output_file: str, functions: List[Function]):
         writer.writerow(csv_header)
         for fun in functions:
             write_function(writer, fun)
-
