c2ecf55ca54b13ac0a501f72c74177b3a7358511
Erik Blokland <me@erikblok.land>
2023-05-02 14:10:58 +0200

    documented trace comparison

--- analysis/energy_testing/compare_full_trace.py
+++ analysis/energy_testing/compare_full_trace.py
@@ -3,6 +3,8 @@ import csv
 from functools import singledispatch
 from typing import Union, Callable, Optional, List
 
+import scipy
+
 from trace_reader_utils.pickle_utils import *
 
 from analysis.function.function import Function
@@ -10,6 +12,14 @@ from analysis.function.function import Function
 
 @singledispatch
 def sum_full_trace(arg, function_filter) -> float:
+    """
+    Returns the sum of the local energy consumption of each Function in a set of Function objects.
+
+    :param arg: Pickle file or dict containing Function objects
+    :param function_filter: Optional filter, receives a Function object and returns true if it should be
+        included in the sum
+    :return: sum of local energy consumption of the Functions in the provided collection
+    """
     raise TypeError(f'Unknown type {type(arg)}')
 
 
@@ -20,14 +30,22 @@ def sum_full_trace_str(pickle_file: str, function_filter: Optional[Callable[[Fun
 
 
 @sum_full_trace.register
-def sum_full_trace_list(funs: dict, function_filter: Optional[Callable[[Function], bool]] = None) -> float:
+def sum_full_trace_dict(funs: dict, function_filter: Optional[Callable[[Function], bool]] = None) -> float:
     if len(funs) == 0 or not isinstance(list(funs.values())[0], Function):
         raise TypeError(f'Need list of Function type, got {type(funs[0])}')
 
-    return sum(fun.energy.local_energy for fun in funs.values() if function_filter is None or function_filter(fun))
+    return sum(fun.energy.local_energy for fun in funs.values() if function_filter is None or not function_filter(fun))
 
 
 def get_sums_from_dir(directory: str, function_filter: Optional[Callable[[Function], bool]] = None) -> List[float]:
+    """
+    Searches a directory for files containing function dictionary pickles and returns a list of the total energy
+    use of each dictionary.
+    :param directory: Directory to search
+    :param function_filter: Optional filter to remove functions from the list.  The filter should return true
+        if the function is to be included in the sum.
+    :return: A list containing the total energy consumption for each of the pickles in the directory.
+    """
     files = os.listdir(directory)
     sums: List[float] = []
     for file in files:
@@ -40,3 +58,18 @@ def get_sums_from_dir(directory: str, function_filter: Optional[Callable[[Functi
         except ValueError:
             pass
     return sums
+
+
+def compare_directories(dir1: str, dir2: str, function_filter: Optional[Callable[[Function], bool]] = None,
+                        test: Callable = scipy.stats.mannwhitneyu):
+    """
+    Compares two directories with the provided test and returns the result
+
+    :param dir1: Directory containing a set of function dict pickles
+    :param dir2: Directory containing a different set of function dict pickles
+    :param test: Test to execute and return the result of.  By default, scipy.stats.mannwhitneyu
+    """
+    dir1_sums = get_sums_from_dir(dir1, function_filter)
+    dir2_sums = get_sums_from_dir(dir2, function_filter)
+    return test(dir1_sums, dir2_sums)
+
